0b00b2b3ecfb79b8a98d27fbf7207d8af77b79a2
NoahY <noahy57@gmail.com>
2011-09-05 18:26:17 +0530

    meta

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,18 +1,18 @@
 <?php
         
 /*              
-        Plugin Name: Signatures
-        Plugin URI: https://github.com/NoahY/q2a-signatures
-        Plugin Description: Signatures
+        Plugin Name: Theme Switcher
+        Plugin URI: https://github.com/NoahY/q2a-theme-switcher
+        Plugin Description: Theme Switcher
         Plugin Version: 0.1
-        Plugin Date: 2011-08-16
+        Plugin Date: 2011-09-05
         Plugin Author: NoahY
         Plugin Author URI:                              
         Plugin License: GPLv2                           
-        Plugin Minimum Question2Answer Version: 1.3
+        Plugin Minimum Question2Answer Version: 1.4
 */                      
                         
-                        
+        
         if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
                         header('Location: ../../');
                         exit;   
