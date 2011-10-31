de58bb5880f0aba6dd3e44d3a64f4028bbeddfa6
NoahY <noahy57@gmail.com>
2011-10-31 13:56:17 +0530

    fixed cookie stuff, notice

--- qa-theme-layer.php
+++ qa-theme-layer.php
@@ -23,20 +23,24 @@
 				);
 			}
 			
-			if($_GET['theme_switch']) {
+			if(@$_GET['theme_switch']) {
+
+			/*
+
 				if($userid = qa_get_logged_in_userid()) {
 					qa_db_query_sub(
 						'INSERT INTO ^usermeta (user_id,meta_key,meta_value) VALUES (#,$,$) ON DUPLICATE KEY UPDATE meta_value=$',
 						$userid,'custom_theme',$_GET['theme_switch'],$_GET['theme_switch']
 					);
 				}
-				else setcookie('qa_theme_switch', $_GET['theme_switch'], time()+86400*365, '/', QA_COOKIE_DOMAIN);
+			*/
+				setcookie('qa_theme_switch', $_GET['theme_switch'], time()+86400*365, '/', QA_COOKIE_DOMAIN);
 				qa_redirect($this->request,array());
 			}
 
 			if (qa_opt('theme_switch_enable')) {
 				
-				if($this->template == 'user') { 
+				if($this->template == 'user' && !qa_get('tab')) { 
 
 					// add theme switcher
 
